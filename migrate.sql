4883f8d0dde24afe6688f854e81fab69055c2098
iwashi623 <wonderland.7878@gmail.com>
2024-01-27 15:23:45 +0900

    add index

--- migrate.sql
+++ migrate.sql
@@ -1,1 +1,4 @@
 ALTER TABLE comments  ADD INDEX comments_post_id_index(post_id);
+
+ALTER TABLE posts ADD INDEX posts_created_at_index(created_at);
+ALTER TABLE comments ADD INDEX posts_user_id_index(user_id);
